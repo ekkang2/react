0ff510733cba315b52b5cc71b24ae9339a39bd5b
chhak0503 <chhak0503@gmail.com>
2024-11-12 12:36:22 +0900

    hook study

--- ch04/src/components/UseReducerComponent.jsx
+++ ch04/src/components/UseReducerComponent.jsx
@@ -1,4 +1,3 @@
-import { useDeferredValue } from "react";
 import { useReducer } from "react";
 
 // 상태 선언
@@ -18,13 +17,78 @@ function countReducer(state, action) {
   }
 }
 
+// 초기 상태 선언
+const initUserState = {
+  uid: "",
+  name: "",
+  age: 0,
+};
+
+// 리듀서 정의
+function userReducer(state, action) {
+  console.log("action.name : " + action.name);
+  console.log("action.value : " + action.value);
+
+  /*
+  const user = { ...state }; // state 복사
+
+  if (action.name === "uid") {
+    user.uid = action.value;
+  }
+
+  if (action.name === "name") {
+    user.name = action.value;
+  }
+
+  if (action.name === "age") {
+    user.age = action.value;
+  }
+    */
+  return { ...state, [action.name]: action.value };
+}
+
 export default function UseReducerComponent() {
   // useReducer(), 사용자 정의 state와 state 처리함수를 정의하는 react hook
-  const [countState, countDispatch] = useReducer(initCountState, countReducer);
+  const [countState, countDispatch] = useReducer(countReducer, initCountState); // 1:리듀서, 2:초기상태
+  const [userState, userDispatch] = useReducer(userReducer, initUserState);
 
   return (
     <div className="UseReducerComponent">
       <h4>UseReducerComponent</h4>
+      <p>count : {countState.count}</p>
+      <button onClick={() => countDispatch({ type: "INCREASE" })}>증가</button>
+      <button onClick={() => countDispatch({ type: "DECREASE" })}>감소</button>
+      <button onClick={() => countDispatch({ type: "RESET" })}>리셋</button>
+
+      <p>
+        아이디 : {userState.uid}
+        <br />
+        이름 : {userState.name}
+        <br />
+        나이 : {userState.age}
+        <br />
+      </p>
+      <input
+        type="text"
+        name="uid"
+        value={userState.uid}
+        onChange={(e) => userDispatch(e.target)}
+      />
+      <br />
+      <input
+        type="text"
+        name="name"
+        value={userState.name}
+        onChange={(e) => userDispatch(e.target)}
+      />
+      <br />
+      <input
+        type="text"
+        name="age"
+        value={userState.age}
+        onChange={(e) => userDispatch(e.target)}
+      />
+      <br />
     </div>
   );
 }
