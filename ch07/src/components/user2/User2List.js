a04b2ba2d76d59714852a4bf800466e9aba53368
chhak0503 <chhak0503@gmail.com>
2024-11-18 12:32:19 +0900

    react http study

--- ch07/src/components/user2/User2List.js
+++ ch07/src/components/user2/User2List.js
@@ -1,25 +1,48 @@
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
+// 사용자 목록 패치함수
+const fetchUsers = async () => {
+  const response = await axios.get("http://localhost:8080/ch09/user2");
+  return response.data;
+};
+
+// 사용자 삭제 함수
+const deleteUser = async (user) => {
+  const response = await axios.delete(
+    `http://localhost:8080/ch09/user2/${user.uid}`
+  );
+  return response.data;
+};
+
 export default function User2List() {
   const navigate = useNavigate();
-  const [users, setUsers] = useState([]);
+  //const [users, setUsers] = useState([]);
 
-  // 컴포넌트 생명주기를 제어하는 훅
-  useEffect(() => {
-    axios
-      .get("http://localhost:8080/ch09/user2")
-      .then((response) => {
-        console.log(response.data);
+  // react-query 사용자 훅
+  const queryClient = useQueryClient();
 
-        // 서버에서 받아온 데이터로 상태 업데이트
-        setUsers(response.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []); // 의존성(반응형) 배열을 빈배열로 선언하면 컴포넌트가 처음 마운트 될때 한번만 실행
+  // react-query 조회 query 함수 정의
+  const {
+    data: users,
+    isLoading,
+    isError,
+  } = useQuery({
+    queryKey: ["users"],
+    queryFn: fetchUsers,
+  });
+
+  // react-query 삭제 mutation 함수 정의
+  const deleteMutation = useMutation({
+    mutationFn: deleteUser,
+    onSuccess: () => {
+      queryClient.invalidateQueries(["users"]);
+    },
+  });
+
+  console.log(users);
 
   const modifyMove = (user) => {
     // 쿼리 파라미터 전송
@@ -27,17 +50,7 @@ export default function User2List() {
   };
 
   const deleteProc = (user) => {
-    axios
-      .delete(`http://localhost:8080/ch09/user2/${user.uid}`)
-      .then((response) => {
-        console.log(response.data);
-
-        // 목록 갱신을 위해서 상태 업데이트
-        setUsers(users.filter((u) => u.uid !== user.uid));
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    deleteMutation.mutate(user);
   };
 
   return (
@@ -54,7 +67,7 @@ export default function User2List() {
           <th>관리</th>
         </tr>
 
-        {users.map((user, index) => (
+        {users?.map((user, index) => (
           <tr key={user.uid}>
             <td>{user.uid}</td>
             <td>{user.name}</td>
